5e2ab3fb09f0c10d0ba1416befb7d5230ef02e55
kelvinma <me@kelvinma.com>
2015-11-09 16:31:56 -0500

    Wrote AJAX for Login Credentials. Test passed.

---
+++ assets/js/grocery_list_api.js
@@ -0,0 +1,83 @@
+'use strict';
+var glapi = {
+
+    gl: 'http://localhost:3000',
+
+    ajax: function(config, cb) {
+    $.ajax(config).done(function(data, textStatus, jqxhr) {
+      cb(null, data);
+    }).fail(function(jqxhr, status, error) {
+      cb({jqxher: jqxhr, status: status, error: error});
+    });
+  },
+
+  register: function register(credentials, callback) {
+    this.ajax({
+      method: 'POST',
+      // url: 'http://httpbin.org/post',
+      url: this.gl + '/users',
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify(credentials),
+      dataType: 'json'
+    }, callback);
+  },
+
+  login: function login(credentials, callback) {
+    this.ajax({
+      method: 'POST',
+      // url: 'http://httpbin.org/post',
+      url: this.gl + '/login',
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify(credentials),
+      dataType: 'json'
+    }, callback);
+  }
+};
+
+$(function() {
+  var form2object = function(form) {
+    var data = {};
+    $(form).find("input").each(function(index, element) {
+      var type = $(this).attr('type');
+      if ($(this).attr('name') && type !== 'submit' && type !== 'hidden') {
+        data[$(this).attr('name')] = $(this).val();
+      }
+    });
+    return data;
+  };
+  var wrap = function wrap(root, formData) {
+    var wrapper = {};
+    wrapper[root] = formData;
+    return wrapper;
+  };
+
+  var callback = function callback(error, data) {
+    if (error) {
+      console.error(error);
+      $('#result').val('status: ' + error.status + ', error: ' +error.error);
+      return;
+    }
+    $('#result').val(JSON.stringify(data, null, 4));
+  };
+
+  $('#register').on('submit', function(e) {
+    var credentials = wrap('credentials', form2object(this));
+    glapi.register(credentials, callback);
+    e.preventDefault();
+  });
+
+  $('#login').on('submit', function(e) {
+    var credentials = wrap('credentials', form2object(this));
+    var cb = function cb(error, data) {
+      if (error) {
+        callback(error);
+        return;
+      }
+      callback(null, data);
+      $('.token').val(data.user.token);
+    };
+    e.preventDefault();
+    glapi.login(credentials, cb);
+  });
+
+});
