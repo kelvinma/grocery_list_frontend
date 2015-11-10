6c5ce6a9bc5cafb506a2af48ed17d9abc0000b18
kelvinma <me@kelvinma.com>
2015-11-10 12:05:22 -0500

    Tweaked layout to accomodate JSON parsing in to HTML + login

--- assets/js/grocery_list_api.js
+++ assets/js/grocery_list_api.js
@@ -44,6 +44,20 @@ var glapi = {
 
     }, callback);
   },
+    createWeeklyMenu: function (token, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.gl + '/weekly_menus',
+      data: { weekly_menu: {
+        week_of: $('#week_of').val() }
+      },
+      headers: {
+        Authorization: 'Token token=' + token
+      },
+      dataType: 'json'
+
+    }, callback);
+  },
     //Authenticated api actions
   showGroceries: function (token, callback) {
     this.ajax({
@@ -55,7 +69,18 @@ var glapi = {
       dataType: 'json'
 
     }, callback);
-  }
+  },
+  logout: function (token, callback) {
+  this.ajax({
+    method: 'DELETE',
+    url: this.gl + '/users/',
+    headers: {
+      Authorization: 'Token token=' + token
+    },
+    dataType: 'json'
+
+  }, callback);
+}
 };
 
 $(function() {
@@ -99,6 +124,7 @@ $(function() {
         callback(error);
         return;
       }
+      $('#login-header').hide(400);
       callback(null, data);
       $('.token').val(data.user.token);
     };
@@ -106,16 +132,68 @@ $(function() {
     glapi.login(credentials, cb);
   });
 
+// Weekly Menu JS
+
+  var showMenus = function(error, data) {
+  if (error) {
+    console.error(error);
+    $('#result').val('status: ' + error.status + ', error: ' +error.error);
+    return;
+  }
+    $('#weeklymenus').html("<p>" + JSON.stringify(data, null, 4));
+  };
+
   $('#show-menus').on('submit', function(e) {
     var token = $(this).children('[name="token"]').val();
     e.preventDefault();
-    glapi.showWeeklyMenus(token, callback);
+    glapi.showWeeklyMenus(token, showMenus);
   });
+
+  $('#create-menu').on('submit',function(e){
+    var token = $(this).children('[name="token"]').val();
+    e.preventDefault();
+    glapi.createWeeklyMenu(token, showMenus)
+  });
+
+
+//Grocery List JS
 
   $('#show-groceries').on('submit', function(e) {
     var token = $(this).children('[name="token"]').val();
     e.preventDefault();
-    glapi.showGroceries(token, callback);
+    var showGroceries = function(error, data) {
+    if (error) {
+      console.error(error);
+      $('#result').val('status: ' + error.status + ', error: ' +error.error);
+      return;
+    }
+    $('#grocerylist').html("<p>" + JSON.stringify(data, null, 4));
+  };
+    glapi.showGroceries(token, showGroceries);
   });
+
+  $('#logout').on('submit', function(e) {
+    // var token = $(this).children('[name="token"]').val();
+    e.preventDefault();
+    glapi.logout(token, callback);
+  });
+
+// Behavior JS
+
+  $('#login').hide();
+
+  $('#loginshow').click(function(e){
+      e.preventDefault();
+      $('#login').show(400);
+      $('#register').hide(400);
+  });
+
+  $('#registershow').click(function(e){
+      e.preventDefault();
+      $('#register').show(400);
+      $('#login').hide(400);
+  });
+
+
 
 });
